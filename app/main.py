4d2166ce2cb64ce02ad64cf1a79be4b3b4ec4bf8
gpspake <gpspake@gmail.com>
2021-06-10 16:16:54 -0500

    add fastapi auth0
    https://www.youtube.com/watch?v=cGRdFjgAy9s
    https://pypi.org/project/fastapi-auth0/

--- app/main.py
+++ app/main.py
@@ -1,16 +1,35 @@
+import os
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Security
+from fastapi_auth0 import Auth0, Auth0User
 import sqlalchemy
 
-app = FastAPI(docs_url='/docs', openapi_prefix='/')
+auth0_domain = os.getenv('AUTH0_DOMAIN')
+auth0_api_audience = os.getenv('AUTH0_API_AUDIENCE')
+
+auth = Auth0(domain=auth0_domain, api_audience=auth0_api_audience, scopes={'read:hello': 'Read Hello'})
+app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": sqlalchemy.__version__}
 
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/secure/hello", dependencies=[Depends(auth.implicit_scheme)])
+def get_secure_hello(user: Auth0User = Security(auth.get_user)):
+    return {"hello": f"{user}"}
+
+
+@app.get("/secure/hello2")
+def get_secure_hello2(user: Auth0User = Security(auth.get_user, scopes=["read:hello"])):
+    return {"hello": f"{user}"}
+
 
 if __name__ == '__main__':
     import uvicorn
