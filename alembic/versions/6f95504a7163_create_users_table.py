8e65870dbf9277e902e454d8ec93d2a22360b1e8
gpspake <gpspake@gmail.com>
2021-06-10 22:10:29 -0500

    add alembic

---
+++ alembic/versions/6f95504a7163_create_users_table.py
@@ -0,0 +1,29 @@
+"""create users table
+
+Revision ID: 6f95504a7163
+Revises: 
+Create Date: 2021-06-11 02:52:14.438122
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '6f95504a7163'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'users',
+        sa.Column('id', sa.String(), nullable=False),
+        sa.Column('auth0_id', sa.String(), nullable=True),
+        sa.Column('email', sa.String(), nullable=True),
+        sa.PrimaryKeyConstraint('id')
+    )
+
+
+def downgrade():
+    op.drop_table('users')
