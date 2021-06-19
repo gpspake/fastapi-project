13cf701df6e76043004972e807a91f7b84da0052
gpspake <gpspake@gmail.com>
2021-06-19 15:59:05 -0500

    specify todo_list_id param type

--- models/user.py
+++ models/user.py
@@ -12,5 +12,5 @@ class PydanticUser(BaseModel):
     class Config:
         orm_mode = True
 
-    def has_todo_list(self, todo_list_id):
+    def has_todo_list(self, todo_list_id: int):
         return any(todo_lists.id == todo_list_id for todo_lists in self.todo_lists)
